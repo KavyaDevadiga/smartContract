3f90eb26585574cf8a01b729a3da6e0351d20492
nithinshetty99 <nithinshettynitthishetty@gmail.com>
2023-03-26 18:51:07 +0530

    error

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,2 +1,21 @@
 const assert = require('assert');
 const Web3 = require('web3');
+const ganache = require('ganache-cli');
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.constract(JSON.parse(interface))
+        .deploy({ data : bytecode, arguments : ['Hi there!']})
+        .send({ from : accounts[0], gas: '1000000'})
+});
+
+describe('Inbox', () => {
+    it('deploys a constract', () => { 
+        console.log(inbox);
+    });
+});
